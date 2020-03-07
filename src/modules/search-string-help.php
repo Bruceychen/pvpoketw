90b51eb810bce12037122d461112397e4fb9bc8c
r08006(Bruce Chen) <r08006@gmail.com>
2020-03-07 23:02:01 +0800

    修正搜尋字串說明頁面

--- src/modules/search-string-help.php
+++ src/modules/search-string-help.php
@@ -1,53 +1,89 @@
-<div class="sandbox-search-strings article hide">
-	<p>使用以下格式可以用來過濾尋找特定條件寶可夢：</p>
+<div class="sandbox-search-strings article hide" style="overflow:auto;">
+	<p>使用以下關鍵字來尋找特定條件寶可夢：</p>
 	<table cellspacing="0">
 		<tr>
-			<td><strong>顯示特定寶可夢 如：</strong></td>
+			<td><strong>寶可夢中文名稱</strong></td>
 			<td>瑪力露麗</td>
 		</tr>
 		<tr>
-			<td><strong>輸入英文屬性名稱 如：</strong></td>
+			<td><strong>寶可夢屬性</strong></td>
 			<td>water</td>
 		</tr>
 		<tr>
-			<td><strong>輸入英文寶可夢類別標籤，有以下選項:</strong></td>
-			<td>legendary(傳說的寶可夢), mythical(幻之寶可夢), regional(所有地區型態), alolan(阿羅拉地區型態), galarian(伽勒爾地區型態), starter(最初拿到的寶可夢、御三家), shadow(只顯示暗影寶可夢), shadoweligible(有暗影化寶可夢，但只顯示一般個體)</td>
+			<td><strong>類別</strong></td>
+            <td>legendary<br><br>
+
+                legendary :<i>傳說的寶可夢</i><br>
+                mythical :<i>幻之寶可夢</i><br>
+                regional :<i>現實世界區域限定</i><br>
+                alolan :<i>阿羅拉型態</i><br>
+                galarian :<i>伽勒爾型態</i><br>
+                starter :<i>初始寶可夢</i><br>
+                shadow :<i>僅暗影寶可夢</i><br>
+                shadoweligible :<i>有暗影化型態，<strong>但只顯示一般個體</strong></i>
+            </td>
 		</tr>
 		<tr>
-			<td><strong>輸入英文 世代 或 地區，有以下選項:</strong></td>
-			<td>gen1, gen2, gen3, gen4, gen5 <br>kanto, johto, hoenn, sinnoh, unova</td>
+			<td>
+                <strong>世代</strong><br>
+            </td>
+			<td>gen1
+            </td>
 		</tr>
+        <tr>
+            <td>
+                <strong>地區</strong>
+            </td>
+            <td>kanto<br><br>
+
+                kanto :<i>關都地區</i><br>
+                johto :<i>城都地區</i><br>
+                hoenn :<i>豐緣地區</i><br>
+                sinnoh :<i>神奧地區</i><br>
+                unova :<i>合眾地區</i><br>
+            </td>
+        </tr>
 		<tr>
-			<td><strong>輸入全國圖鑑編號</strong></td>
+			<td><strong>圖鑑編號</strong></td>
 			<td>184</td>
 		</tr>
 		<tr>
-            <td><strong>輸入中文招式名稱 如：</strong></td>
+            <td><strong>招式名稱</strong></td>
             <td>@雙倍奉還</td>
 		</tr>
 		<tr>
-            <td><strong>輸入英文招式屬性名稱 如：</strong></td>
+            <td><strong>招式屬性</strong></td>
             <td>@fighting</td>
 		</tr>
 		<tr>
-            <td><strong>顯示絕版招式(不含 報恩) 如：</strong></td>
-            <td>@Legacy</td>
+            <td>
+                <strong>限定招式</strong>
+            </td>
+            <td>
+                @Legacy<br>
+                (不含遷怒、報恩)
+            </td>
         </tr>
         <tr>
-            <td><strong>顯示所有特殊招式(包含 絕版技能、報恩) 如：</strong></td>
-            <td>@special</td>
+            <td>
+                <strong>限定招式</strong><br>
+            </td>
+            <td>
+                @special<br>
+                <strong>(含遷怒、報恩)</strong>
+            </td>
         </tr>
         <tr>
-			<td><strong>And(且)符號 如：</strong></td>
-			<td>"water&amp;@fighting"</td>
+			<td><strong>And(且)</strong></td>
+			<td>water&amp;@fighting</td>
 		</tr>
 		<tr>
-			<td><strong>Or(或)符號 如：</strong></td>
-			<td>"water,fighting"</td>
+			<td><strong>Or(或)</strong></td>
+			<td>water,fighting</td>
 		</tr>
 		<tr>
-			<td><strong>Not(非)符號 如：</strong></td>
-			<td>"!water"</td>
+			<td><strong>Not(非)</strong></td>
+			<td>!water</td>
 		</tr>
 	</table>
 </div>

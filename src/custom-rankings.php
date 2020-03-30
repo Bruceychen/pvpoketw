4c27072a2d64da29a601758ea3dc6bd44c1c1cf2
Bruce Chen <r08006@gmail.com>
2020-03-31 02:27:32 +0800

    修正錯誤

--- src/custom-rankings.php
+++ src/custom-rankings.php
@@ -319,5 +319,6 @@ require_once 'header.php'; ?>
 <script src="<?php echo $WEB_ROOT; ?>js/interface/PokeMultiSelect.js?v=<?php echo $SITE_VERSION; ?>"></script>
 <script src="<?php echo $WEB_ROOT; ?>js/interface/CustomRankingInterface.js?v=<?php echo $SITE_VERSION; ?>"></script>
 <script src="<?php echo $WEB_ROOT; ?>js/interface/ModalWindow.js?v=<?php echo $SITE_VERSION; ?>"></script>
-
+<!--pvpoketw tools-->
+<script src="<?php echo $WEB_ROOT; ?>js/interface/TWTools.js?v=<?php echo $SITE_VERSION; ?>"></script>
 <?php require_once 'footer.php'; ?>

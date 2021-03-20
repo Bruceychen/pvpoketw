2e806a70d6f685033be0d535e842d97db3de4570
Bruce Chen <r08006@gmail.com>
2021-03-20 13:41:25 +0800

    新增四體 XL寶可夢資料; 大師聯盟拆分成經典賽與一般大師聯盟

--- src/modules/formatselect.php
+++ src/modules/formatselect.php
@@ -2,6 +2,7 @@
 	<option value="1500" cup="all">超級聯盟</option>
 	<option value="2500" cup="all">高級聯盟</option>
 	<option value="10000" cup="all">大師聯盟</option>
+    <option value="10000" cup="classic">大師聯盟 (經典賽)</option>
 	<option value="2500" cup="premier">紀念盃 (高級)</option>
     <option value="1500" cup="retro">復古盃</option>
     <option value="1500" cup="vortex">Silph 漩渦盃</option>

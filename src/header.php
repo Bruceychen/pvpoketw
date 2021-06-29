7328a07f38de95910b452c888cd2ddd1e72c5579
Bruce Chen <r08006@gmail.com>
2021-06-29 09:16:00 +0800

    update

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.22.12.5.1';
+$SITE_VERSION = '1.22.12.9';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {

cb7f9c80bd14806ae3280980338f8ca2544d0bbc
Bruce Chen <r08006@gmail.com>
2021-10-01 10:56:34 +0800

    移除高級Remix選單項目、蓋諾賽克特 水流卡帶追加 高科技光砲 水 限定招式、排名資料易動

--- src/modules/cupselect.php
+++ src/modules/cupselect.php
@@ -1,7 +1,6 @@
 <select class="format-select">
 	<option value="all" cup="all">全部寶可夢</option>
 	<option value="official" cup="premier">紀念盃</option>
-    <option value="official" cup="remix">Remix盃</option>
     <option value="official" cup="halloween">萬聖節盃</option>
     <option value="official" cup="littlejungle">叢林小小盃</option>
     <option value="tsa-3" cup="factions">Silph Factions (Dungeon)</option>

3c028879fda4d67793d01be0df54b968b6015291
Bruce Chen <r08006@gmail.com>
2022-02-08 10:16:00 +0800

    Code alignment with origin site til commit: 1b76d9d598a060fb860cd0227847c389f74756f4 Mon Feb 7 17:49:38 2022 -0600

--- src/modules/formatselect.php
+++ src/modules/formatselect.php
@@ -1,11 +1,11 @@
 <select class="format-select">
 	<option value="1500" cup="all" meta-group="great">超級聯盟</option>
 	<option value="2500" cup="all" meta-group="ultra">高級聯盟</option>
-    <option value="2500" cup="premierclassic" meta-group="ultrapremierclassic">UL紀念經典賽</option>
 	<option value="10000" cup="all" meta-group="master">大師聯盟</option>
 	<option value="10000" cup="classic" meta-group="master">大師聯盟 (經典賽)</option>
     <option value="10000" cup="premierclassic" meta-group="master">ML紀念經典賽</option>
     <option value="1500" cup="love" meta-group="love">Love Cup</option>
+    <option value="1500" cup="johto" meta-group="johto">城都盃</option>
     <option value="1500" cup="guardian" meta-group="guardian">Silph 守護者盃</option>
     <option value="1500" cup="factions" meta-group="factions">Silph Factions (Cave)</option>
     <option value="1500" cup="fusionfactions" meta-group="fusion">Silph Factions (Fusion)</option>

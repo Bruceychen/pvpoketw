6ef4fb5823244c978f1d86b3cb1a6cb0c4947a8e
Bruce Chen <r08006@gmail.com>
2022-04-23 12:47:59 +0800

    Code alignment til commit: ffbdf0026f197d543a0b182ea69519c0c1a2fe16 Fri Apr 22 16:20:59 2022 -0500
    穿著熊 加入 Silph五月主題賽 Firefly Cup 排名資料

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.27.1.4';
+$SITE_VERSION = '1.27.1.5';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {
@@ -114,7 +114,7 @@ if(! isset($OG_IMAGE)){
 <?php endif; ?>
 
 <?php if(strpos($_SERVER['REQUEST_URI'], 'articles') !== false): ?>
-	<link rel="stylesheet" type="text/css" href="<?php echo $WEB_ROOT; ?>css/article-extras.css?v=3">
+	<link rel="stylesheet" type="text/css" href="<?php echo $WEB_ROOT; ?>css/article-extras.css?v=4">
 <?php endif; ?>
 
 <?php if((isset($_SETTINGS->theme))&&($_SETTINGS->theme != "default")): ?>

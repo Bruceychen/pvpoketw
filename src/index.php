56e3d07627b0150644ae0f1ceec05042131eb35a
Bruce Chen <r08006@gmail.com>
2022-06-15 10:23:09 +0800

    Code alignment with origin til commit: 10dfa59405b1f9cf8d3364270367350380428a55 Tue Jun 14 18:09:15 2022 -0500
        Updated Crabominable and Dusk Lycanroc movesets

--- src/index.php
+++ src/index.php
@@ -62,14 +62,14 @@
 
 	<div class="article-item flex">
 		<div class="col-3">
-			<a href="<?php echo $WEB_ROOT; ?>articles/infographics/22-06-season-11-ultra-league-starter-pack/">
-				<img src="<?php echo $WEB_HOST_ORIGN; ?>articles/article-assets/infographics/22-06-season-11-ultra-league-starter-pack/thumb.jpg">
+			<a href="<?php echo $WEB_ROOT; ?>articles/infographics/22-06-season-11-master-league-starter-pack/">
+				<img src="<?php echo $WEB_HOST_ORIGN; ?>articles/article-assets/infographics/22-06-season-11-master-league-starter-pack/thumb.jpg">
 			</a>
 		</div>
 		<div class="col-9">
-			<h4><a href="<?php echo $WEB_ROOT; ?>articles/infographics/22-06-season-11-ultra-league-starter-pack/">Ultra League Starter Pack for Season 11</a></h4>
-			<div class="date">June 6, 2022</div>
-			<p>Check out the premiere and budget picks to get started in Ultra League for GO Battle League Season 11!</p>
+			<h4><a href="<?php echo $WEB_ROOT; ?>articles/infographics/22-06-season-11-master-league-starter-pack/">Master League Starter Pack for Season 11</a></h4>
+			<div class="date">June 14, 2022</div>
+			<p>Check out the best legendary and non-legendary Pokemon to get started in Master League for GO Battle League Season 11!</p>
 			<div class="tags"><a href="<?php echo $WEB_ROOT; ?>articles?tag=Infographic"># Infographic</a><a href="<?php echo $WEB_ROOT; ?>articles?tag=Starter Pack"># Starter Pack</a></div>
 		</div>
 	</div>
@@ -102,8 +102,8 @@
     <p>Brucey</p>
     <h3>最後更新時間</h3>
             <ul>
-                <li>中文版最後更新時間：Tue Jun 14 08:25:28 2022 +0800</li>
-                <li>原版最後版本時間：Mon Jun 13 18:36:43 2022 -0500</li>
+                <li>中文版最後更新時間：Wed Jun 15 08:25:28 2022 +0800</li>
+                <li>原版最後版本時間：Tue Jun 14 18:09:15 2022 -0500</li>
             </ul>
 </div>
 

b224f65d5670dd903393cb6032db4e72209f88bc
Bruce Chen <r08006@gmail.com>
2023-03-02 10:22:11 +0800

    Code alignment with origin site til commit cd60cb599d6f96ecbe159982fbe07216d7e43520 Wed Mar 1 14:20:09 2023 -0600     Season of Rising Heroes moveset and ranking updates

--- src/js/interface/PokeSelect.js
+++ src/js/interface/PokeSelect.js
@@ -321,6 +321,8 @@ function PokeSelect(element, i){
 					}
 				}
 
+				$el.find(".form-link").hide();
+
 				if(forms.length > 0){
 					$el.find(".form-select-container").css("visibility", "visible");
 

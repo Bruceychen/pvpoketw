0d29bb658bd98c3cd823c067cf37ec3a0c27ae3a
Bruce Chen <r08006@gmail.com>
2023-03-28 10:59:28 +0800

    Code alignment with origin site til commit 37f50ef461433d51939d8dce80182a24f7e800bc Mon Mar 27 13:51:27 2023 -0500     Searchable team alternatives     Added searchbar and functionality in the Potential Alternatives section of the Team Builder. Split alternatives display into its own function.

---
+++ src/modules/search-traits.php
@@ -0,0 +1,9 @@
+<div class="search-traits-selector hide">
+	<p>選擇以下寶可夢對戰特質(traits)與風格(playstyles)進行搜尋.</p>
+
+	<div class="traits"></div>
+
+	<div class="center flex">
+		<div class="button search">搜尋</div>
+	</div>
+</div>

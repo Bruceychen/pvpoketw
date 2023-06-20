c117f8b19ed154126e4fa95ebe2f8271c8c711d7
Bruce Chen <r08006@gmail.com>
2023-06-20 13:08:15 +0800

    Code alignment with origin site til commit 4d5bbbde0c66fcf999eb938f832005fb7639d029 Mon Jun 19 14:55:49 2023 -0500     Retro Cup rankings

--- src/custom-rankings.php
+++ src/custom-rankings.php
@@ -118,6 +118,7 @@ require_once 'header.php'; ?>
                     <div class="check" value="galarian"><span></span> 伽勒爾型態</div>
                     <div class="check" value="hisuian"><span></span> 洗翠型態</div>
 					<div class="check" value="regional"><span></span> 地區限定</div>
+                    <div class="check" value="starter"><span></span> Starter</div>
                     <div class="check" value="shadow"><span></span>暗影化</div>
                     <div class="check" value="shadoweligible"><span></span>另有暗影化型態</div>
                     <div class="check" value="mega"><span></span> Mega進化</div>

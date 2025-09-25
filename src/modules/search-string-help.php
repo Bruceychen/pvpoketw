0914402be96510d72b474dc5c3790cf15d7c00f5
brucechen <r08006@gmail.com>
2025-09-25 09:58:56 +0800

    Code alignment with orign site til commit: dc7493d9738f55978aa0cd26f1cbbe5895ef6476 Wed Sep 24 17:26:09 2025 -0500     Editor notes icon in ranking detail header     Added notes icon next to header in ranking details to make it clearer what the icon refers to.

--- src/modules/search-string-help.php
+++ src/modules/search-string-help.php
@@ -1,129 +1,94 @@
 <div class="sandbox-search-strings article hide">
-    <p>使用以下關鍵字來尋找特定條件寶可夢：</p>
-    <table cellspacing="0">
-        <tr>
-            <td><strong>寶可夢中文名稱</strong></td>
-            <td>瑪力露麗</td>
-        </tr>
-        <tr>
-            <td><strong>Pokemon Nickname</strong></td>
-            <td>gfisk (<a class="nickname-list" href="#">List of nicknames<a>)</td>
-        </tr>
-        <tr>
-            <td><strong>寶可夢屬性</strong></td>
-            <td>water:水屬</td>
-        </tr>
-        <tr>
-            <td><strong>類別</strong></td>
-            <td>
-                legendary :<i>傳說的寶可夢</i><br>
-                mythical :<i>幻之寶可夢</i><br>
-                ultrabeast :<i>究極異獸</i><br>
-                regional :<i>現實世界區域限定</i><br>
-                alolan :<i>阿羅拉型態</i><br>
-                galarian :<i>伽勒爾型態但不含伽勒爾限定</i><br>
-                galar :<i>伽勒爾地區限定</i><br>
-                starter :<i>初始寶可夢</i><br>
-                shadow :<i>僅暗影寶可夢</i><br>
-                shadoweligible :<i>有暗影化型態，<strong>但只顯示一般個體</strong></i><br>
-                xl:<i>等級50級的寶可夢</i>
-            </td>
-        </tr>
-        <tr>
-            <td><strong>世代/地區</strong><br></td>
-            <td>
-                gen1:<i>第一世代</i><br>
-                gen2:<i>第二世代</i><br>
-                kanto :<i>關都地區</i><br>
-                johto :<i>城都地區</i><br>
-            </td>
-        </tr>
-        <tr>
-            <td><strong>全國圖鑑編號</strong></td>
-            <td>184</td>
-        </tr>
-        <tr>
-            <td><strong>Evolution Family</strong></td>
-            <td>+politoed</td>
-        </tr>
-        <tr>
-            <td><strong>招式名稱</strong></td>
-            <td>@雙倍奉還</td>
-        </tr>
-        <tr>
-            <td><strong>招式屬性</strong></td>
-            <td>@fighting</td>
-        </tr>
-        <tr>
-            <td><strong>Fast Move only</strong></td>
-            <td>@1fighting</td>
-        </tr>
-        <tr>
-            <td><strong>Charged Move only</strong></td>
-            <td>@2mud</td>
-        </tr>
-        <tr>
-            <td>
-                <strong>限定招式</strong>
-            </td>
-            <td>
-                @Legacy<br>
-                (不含遷怒、報恩)
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <strong>限定招式</strong><br>
-            </td>
-            <td>
-                @special<br>
-                <strong>(含遷怒、報恩)</strong>
-            </td>
-        </tr>
-        <tr>
-            <td><strong>對戰特質(Traits)</strong></td>
-            <td>Bulky, Spammy, Risky, etc. See adjacent trait guide.</td>
-        </tr>
-        <tr>
-            <td><strong>第二特殊招式解鎖星塵數</strong></td>
-            <td>10k,<br><br>
-
-                10k :<i>10000星塵</i><br>
-                50k :<i>50000星塵</i><br>
-                75k :<i>75000星塵</i><br>
-                100k :<i>100000星塵</i><br>
-            </td>
-        </tr>
-        <tr>
-            <td><strong>夥伴寶可夢行走距離</strong></td>
-            <td>1km<br><br>
-
-                1km :<i>1公里</i><br>
-                3km :<i>3公里</i><br>
-                5km :<i>5公里</i><br>
-                20km :<i>20公里</i><br>
-            </td>
-        </tr>
-        <tr>
-            <td><strong>Meta 寶可夢</strong></td>
-            <td>meta</td>
-        </tr>
-        <tr>
-            <td><strong>4* 寶可夢</strong></td>
-            <td>4*, Hundo</td>
-        </tr>
-        <tr>
-            <td><strong>And(且)</strong></td>
-            <td>water&amp;@fighting</td>
-        </tr>
-        <tr>
-            <td><strong>Or(或)</strong></td>
-            <td>water,fighting</td>
-        </tr>
-        <tr>
-            <td><strong>Not(非)</strong></td>
-            <td>!water</td>
-        </tr>
+	<p>使用以下關鍵字來尋找特定條件寶可夢：</p>
+	<table cellspacing="0">
+		<tr>
+			<td><strong>寶可夢中文名稱</strong></td>
+			<td>瑪力露麗</td>
+		</tr>
+		<tr>
+		    <td><strong>寶可夢英文暱稱</strong></td>
+		    <td>gfisk (<a class="nickname-list" href="#">英文暱稱列表<a>)</td>
+		</tr>
+		<tr>
+		    <td><strong>寶可夢屬性</strong></td>
+		    <td>water:水屬</td>
+		</tr>
+		<tr>
+		    <td><strong>類別</strong></td>
+		    <td>legendary :<i>傳說的寶可夢</i><br>mythical :<i>幻之寶可夢</i><br>ultrabeast :<i>究極異獸</i><br>regional :<i>現實世界區域限定</i><br>alolan :<i>阿羅拉型態</i><br>galarian :<i>伽勒爾型態但不含伽勒爾限定</i><br>galar :<i>伽勒爾地區限定</i><br>starter :<i>初始寶可夢</i><br>shadow :<i>僅暗影寶可夢</i><br>shadoweligible :<i>有暗影化型態，<strong>但只顯示一般個體</strong></i><br>xl:<i>等級50級的寶可夢</i></td>
+		</tr>
+		<tr>
+		    <td><strong>世代/地區</strong><br></td>
+		    <td>gen1:<i>第一世代</i><br>gen2:<i>第二世代</i><br>kanto :<i>關都地區</i><br>johto :<i>城都地區</i><br></td>
+		</tr>
+		<tr>
+		    <td><strong>全國圖鑑編號</strong></td>
+		    <td>184</td>
+		</tr>
+		<tr>
+		    <td><strong>Evolution Family</strong></td>
+		    <td>+politoed</td>
+		</tr>
+		<tr>
+		    <td><strong>招式名稱</strong></td>
+		    <td>@雙倍奉還</td>
+		</tr>
+		<tr>
+		    <td><strong>招式屬性</strong></td>
+		    <td>@fighting</td>
+		</tr>
+		<tr>
+		    <td><strong>僅一般招式</strong></td>
+		    <td>@1fighting</td>
+		</tr>
+		<tr>
+		    <td><strong>僅特殊招式</strong></td>
+		    <td>@2mud</td>
+		</tr>
+		<tr>
+		    <td><strong>限定招式</strong></td>
+		    <td>@Legacy<br>(不含遷怒、報恩)</td>
+		</tr>
+		<tr>
+		    <td><strong>限定招式</strong><br></td>
+		    <td>@special<br><strong>(含遷怒、報恩)</strong></td>
+		</tr>
+		<tr>
+		    <td><strong>對戰特質(Traits)</strong></td>
+		    <td>Bulky, Spammy, Risky, etc. See adjacent trait guide.</td>
+		</tr>
+		<tr>
+		    <td><strong>第二特殊招式解鎖星塵數</strong></td>
+		    <td>10k,<br><br>10k :<i>10000星塵</i><br>50k :<i>50000星塵</i><br>75k :<i>75000星塵</i><br>100k :<i>100000星塵</i><br></td>
+		</tr>
+		<tr>
+		    <td><strong>夥伴寶可夢行走距離</strong></td>
+		    <td>1km<br><br>1km :<i>1公里</i><br>3km :<i>3公里</i><br>5km :<i>5公里</i><br>20km :<i>20公里</i><br></td>
+		</tr>
+		<tr>
+		    <td><strong>Meta 寶可夢</strong></td>
+		    <td>meta</td>
+		</tr>
+		<tr>
+		    <td><strong>Editor Notes</strong></td>
+		    <td>notes</td>
+		</tr>
+		<tr>
+		    <td><strong>4* 寶可夢</strong></td>
+		    <td>4*, Hundo</td>
+		</tr>
+		<tr>
+		    <td><strong>And(且)</strong></td>
+		    <td>water&amp;@fighting</td>
+		</tr>
+		<tr>
+		    <td><strong>Or(或)</strong></td>
+		    <td>water,fighting</td>
+		</tr>
+		<tr>
+		    <td><strong>Not(非)</strong></td>
+		    <td>!water</td>
+		</tr>
     </table>
 </div>
 

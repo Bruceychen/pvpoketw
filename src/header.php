d34d5a4306f57481b55a4f10aadaf0ea5323e2af
brucechen <r08006@gmail.com>
2026-07-16 21:42:15 +0800

    code alignment with origin site til commit: 10fd1a6e43260e59b625d1cf96bbea496672880d Wed Jul 15 17:05:23 2026 -0500     Mega Raichu X & Y stats, Dive stats, updated Cramorant moveset

--- src/header.php
+++ src/header.php
@@ -1,5 +1,5 @@
 <?php require_once 'modules/config.php';
-$SITE_VERSION = '1.37.3.24';
+$SITE_VERSION = '1.37.3.25';
 
 // This prevents caching on local testing
 if (strpos($WEB_ROOT, 'src') !== false) {

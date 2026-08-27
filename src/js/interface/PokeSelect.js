992063705ea69e5e7bd7fda80e21b9c97ec10a7e
brucechen <r08006@gmail.com>
2026-08-28 07:17:03 +0800

    code alignment with origin site til commit: 46bd08a777c798481ced7dd1048f3a6bf4ff42f2 Thu Aug 27 13:23:48 2026 -0500     Mega GL, UL, ML rankings, quick form selection in Pokeselect     Battle defaults are temporarily set to "mega" cup.  Search in Pokeselect improved with shorthand to select megas, shadows, and normal forms.

--- src/js/interface/PokeSelect.js
+++ src/js/interface/PokeSelect.js
@@ -1104,6 +1104,25 @@ function PokeSelect(element, i){
 			return;
 		}
 
+		// Use "sab s", "char mx", etc as shortcuts for shadow and mega pokemon
+
+		let idSuffix = null;
+		let shorthands = [
+			{ string: " m", suffix: "_mega" },
+			{ string: " x", suffix: "_mega_x" },
+			{ string: " y", suffix: "_mega_y" },
+			{ string: " s", suffix: "_shadow" },
+			{ string: " n", suffix: "" },
+		];
+
+		shorthands.forEach(shorthand => {
+			if(searchStr.endsWith(shorthand.string)){
+				idSuffix = shorthand.suffix;
+				searchStr = searchStr.substring(0, searchStr.length - shorthand.string.length);
+				return;
+			}
+		});
+
 		var idToSelect;
 
 		for(var i = 0; i < searchArr.length; i++){
@@ -1141,13 +1160,18 @@ function PokeSelect(element, i){
 
 		}
 
+		// Select mega or shadow form of top result
+		if(typeof idSuffix == "string" && idToSelect){
+			idToSelect = idToSelect.replace('_shadow', '').replace('_mega', '') + idSuffix;
+		}
+
 		var idAlreadySelected = false;
 
 		if(selectedPokemon && (idToSelect == selectedPokemon.speciesId)){
 			idAlreadySelected = true;
 		}
 
-		if((idToSelect)&&(! idAlreadySelected)){
+		if(idToSelect && ! idAlreadySelected && searchArr.find(poke => poke.speciesId == idToSelect)){
 			self.setPokemon(idToSelect);
 		}
 	}
@@ -1611,7 +1635,7 @@ function PokeSelect(element, i){
 	$el.find("a.search-info").click(function(e){
 		e.preventDefault();
 
-		modalWindow("Keyboard Commands", $el.find(".pokeselector-search-help"));
+		modalWindow("Search Tips", $el.find(".pokeselector-search-help"));
 	});
 
 	// Open the iv checker modal window
